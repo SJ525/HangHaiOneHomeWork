53007ee391f5e2b413c4ac498654469d5e00bc8b
hsj <1461822113@qq.com>
2019-12-27 17:16:26 +0800

    spring-crud项目初始提交

---
+++ src/main/java/cn/crud/biz/UserInfoBiz.java
@@ -0,0 +1,21 @@
+package cn.crud.biz;
+
+import cn.crud.pojo.UserInfo;
+
+import java.util.List;
+
+/**
+ * 用户业务接口
+ */
+public interface UserInfoBiz {
+    //查询所有用户
+    public List<UserInfo> findAll();
+    //条件查询
+
+    //新增用户
+    public void save(UserInfo userInfo);
+    //修改用户
+
+    //删除用户
+
+}
